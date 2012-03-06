6abac773037d23d6c4ec5c4ad202e20cba31648e
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-03-06 22:00:10 +0100

    Moved all dumberfarmer related code to that file

--- scripts/maps/startingfarm.lua
+++ scripts/maps/startingfarm.lua
@@ -4,23 +4,7 @@
 
 --]]
 
-require "scripts/npcs/walkingnpc"
-
-function initfunction()
-    local dumb_farmer = create_npc("Dumb Farmer", 215, GENDER_MALE, 26 * TILESIZE + TILESIZE / 2, 26 * TILESIZE + TILESIZE / 2, talkToDumbFarmer, updateDumbFarmer)
-    local farmers_way = {
-            {x=27 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2},
-            {x=45 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2},
-            {x=45 * TILESIZE + TILESIZE / 2, y= 37 * TILESIZE + TILESIZE / 2},
-            {x=44 * TILESIZE + TILESIZE / 2, y= 37 * TILESIZE + TILESIZE / 2},
-            {x=44 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2},
-            {x=35 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2},
-            {x=27 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2},
-            {x=27 * TILESIZE + TILESIZE / 2, y= 35 * TILESIZE + TILESIZE / 2},
-            {x=27 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2}}
-    setWaypoints(dumb_farmer, farmers_way, 3, gotoNextWaypoint)
-    gotoNextWaypoint(dumb_farmer)
-end
-
-atinit(initfunction)
+atinit(function()
+    require "scripts/npcs/dumbfarmer"
+end)
 
--- scripts/npcs/dumbfarmer.lua
+++ scripts/npcs/dumbfarmer.lua
@@ -1,9 +1,29 @@
+--[[
 
-function talkToDumbFarmer(npc, ch)
+    A dumb farmer.
+
+--]]
+
+require "scripts/npcs/walkingnpc"
+
+local function dumbFarmerTalk(npc, ch)
     --do_message(npc, ch, "Hello!")
 end
 
-function updateDumbFarmer(npc)
+local dumb_farmer_way = { 
+        {x=27 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2}, 
+        {x=45 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2}, 
+        {x=45 * TILESIZE + TILESIZE / 2, y= 37 * TILESIZE + TILESIZE / 2}, 
+        {x=44 * TILESIZE + TILESIZE / 2, y= 37 * TILESIZE + TILESIZE / 2}, 
+        {x=44 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2}, 
+        {x=35 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2}, 
+        {x=27 * TILESIZE + TILESIZE / 2, y= 51 * TILESIZE + TILESIZE / 2}, 
+        {x=27 * TILESIZE + TILESIZE / 2, y= 35 * TILESIZE + TILESIZE / 2}, 
+        {x=27 * TILESIZE + TILESIZE / 2, y= 26 * TILESIZE + TILESIZE / 2}} 
 
-end
-
+local dumb_farmer = create_npc("Dumb Farmer", 215, GENDER_MALE,
+                               26 * TILESIZE + TILESIZE / 2,
+                               26 *           TILESIZE + TILESIZE / 2,
+                               dumbFarmerTalk, nil)
+setWaypoints(dumb_farmer, dumb_farmer_way, 3, gotoNextWaypoint)
+gotoNextWaypoint(dumb_farmer)
