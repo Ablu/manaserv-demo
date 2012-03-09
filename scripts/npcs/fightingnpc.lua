30508b1a9f15a27e4caa62d4ed05e7db8f422abb
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-03-09 22:11:52 +0100

    Added script for fighting npcs
    Following does not work 100%

---
+++ scripts/npcs/fightingnpc.lua
@@ -0,0 +1,67 @@
+--[[
+
+    Script for letting NPC automatically attack other beings
+
+--]]
+
+module("guard", package.seeall)
+
+local guards = {}
+
+local function isEnemy(npc, being)
+    if guards[npc].filter ~= nil then
+        return guards[npc].filter(npc, being)
+    else
+        return mana.being_type(being) == TYPE_MONSTER
+    end
+end
+
+function create(npc, x, y, strayRadius, damage, damageDelta, damageRange,
+                damageAccurracy, damageType, damageElement, filter,
+                attackCallback)
+    guards[npc] = {
+        x=x,
+        y=y,
+        strayRadius=strayRadius,
+        damage=damage,
+        damageDelta=damageDelta,
+        damageRange=damageRange,
+        damageAccuracy=damageAccurracy,
+        damageType=damageType,
+        damageElement=damageElement,
+        filter=filter,
+        attackCallback=attackCallback
+    }
+end
+
+function lookForEnemy(npc)
+    local guard = guards[npc]
+    assert(guard ~= nil)
+    local beings = mana.get_beings_in_circle(guard.x, guard.y, guard.strayRadius)
+    local closestDist = 0
+    local closestBeing = nil
+    for _, being in pairs(beings) do
+        if isEnemy(npc, being) and
+            (closestBeing == nil or mana.get_distance(npc, being) < closestDist)
+                then
+            closestBeing = being
+            closestDist = mana.get_distance(npc, being)
+        end
+    end
+    if closestBeing == nil then
+        return false
+    end
+
+    if mana.get_distance(npc, closestBeing) <= guard.damageRange then
+        local hpLoss = mana.being_damage(closestBeing, guard.damage,
+          guard.damageDelta, guard.damageAccuracy, guard.damageType,
+          guard.damageElement)
+        mana.being_set_action(npc, ACTION_ATTACK)
+        if guard.attackCallback ~= nil then
+            guard.attackCallback(hpLoss)
+        end
+    else
+        local x, y = mana.posX(closestBeing), mana.posY(closestBeing)
+        mana.being_walk(npc, x, y, 6)
+    end
+end
