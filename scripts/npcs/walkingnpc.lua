a5866d8d9cce4a4da141e694c1ad95411fe6f14c
Stefan Beller <stefanbeller@googlemail.com>
2012-03-10 13:44:13 +0100

    Improvements to walkingnpc generic
    comments and retrying to reach its destination upon unsuccessful callbacks

--- scripts/npcs/walkingnpc.lua
+++ scripts/npcs/walkingnpc.lua
@@ -19,6 +19,19 @@ local function getWalkTime(being, x, y)
     return dist / speed
 end
 
+--- Setup the walking route of an npc.
+-- The npc will walk along the given route, and restart from first waypoint after the last is reached.
+-- @param npc The npc which should move along a route
+-- @param points An array with tables as entries. One entry should have
+--               x=integer in pixel coordinates, y=integer in pixel coordinates,
+--               time=float in seconds, how long to wait upon arrival.
+--               id=string this id will be additionally passed to callback
+-- @param walkspeed The speed this npc should use
+-- @param callback Function to be called when the next waypoint is reached.
+--                 Easiest case would be 'function() gotoNextWaypoint(npc) end'
+--                 to keep the npc walking to the next waypoint after arriving
+--                 the current waypoint.
+
 function setWaypoints(npc, points, walkspeed, callback)
     assert(npc ~= nil, "nil npc handle")
     assert(points ~= nil, "nil point table")
@@ -75,7 +88,6 @@ end
 --           pairs of stopWalking and continueWalking can be issues at
 --           the same time. The npc will only continueWalking if all
 --           characters have issued to continueWalking or have disconnected
-
 function continueRoute(npc, ch)
     assert(waypoints[npc] ~= nil, "nil npc handle")
     local wp = waypoints[npc]
@@ -95,11 +107,28 @@ function continueRoute(npc, ch)
     schedule_in(time, function() walkingCallback(npc) end)
 end
 
+
+--- Internal callback when the npc should have arrived at its destination.
+-- checks if the npc is really there, if not just try again to walk to the right place.
+-- This also handles the user callback
 function walkingCallback(npc)
     local wp = waypoints[npc]
-    if wp.data[wp.currentIndex].x == mana.posX(npc)
-            and wp.data[wp.currentIndex].y == mana.posY(npc)
-            and not next(wp.stoppedBy) then
-        wp.callback(npc)
+    mana.being_say(npc, "walkingCallback")
+    -- check if the npc already reched the waypoint.
+    -- This may be not the case, if the npc was hindered by players to walk there.
+    if wp.data[wp.currentIndex].x == mana.posX(npc) and wp.data[wp.currentIndex].y == mana.posY(npc) then
+        if not next(wp.stoppedBy) then
+            -- In case the npc is being talked to at the moment, we will drop,
+            -- this current callback, since it will be rescheduled again after talking is finished.
+            wp.callback(npc, wp.data[wp.currentIndex].id)
+        end
+    else
+        -- if the  npc is not there, just try again to walk to the destination.
+        mana.being_walk(npc, wp.data[wp.currentIndex].x, wp.data[wp.currentIndex].y,
+                    mana.being_get_modified_attribute(npc, ATTRIBUTE_MOVEMENT_SPEED))
+
+        local time = getWalkTime(npc, wp.data[wp.currentIndex].x,
+                                      wp.data[wp.currentIndex].y)
+        schedule_in(time, function() walkingCallback(npc) end)
     end
 end
