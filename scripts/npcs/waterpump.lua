d196c0947841e004a38f8b3abc72ce2729ee13be
Stefan Beller <stefanbeller@googlemail.com>
2012-03-13 21:06:07 +0100

    adding waterpunp

---
+++ scripts/npcs/waterpump.lua
@@ -0,0 +1,17 @@
+--[[
+
+    A dumb farmer.
+
+--]]
+
+local function waterpumpTalk(npc, ch)
+    do_message(npc, ch, "How many water bottles would you like to fill?")
+    local haveBottles = mana.chr_inv_count(ch, true, true, "Empty Bottle")
+    amount = do_ask_integer(npc, ch, 0, haveBottles, 1)
+    mana.chr_inv_change(ch, "Empty Bottle", -amount, "Bottle of Water", amount)
+end
+
+local water_pump = create_npc("Water Pump", 203, GENDER_UNSPECIFIED,
+                               tileToPixel(64), tileToPixel(41),
+                               waterpumpTalk, nil)
+
