1436e75dd3dcb0c11c6731008dd4376ef74426a8
Stefan Beller <stefanbeller@googlemail.com>
2012-03-16 22:00:45 +0100

    Trying to make hungori fight

--- scripts/npcs/hungori.lua
+++ scripts/npcs/hungori.lua
@@ -7,7 +7,10 @@
 
 require "scripts/npcs/walkingnpc"
 
+require "scripts/npcs/fightingnpc"
+
 local function hungoriTalk(npc, ch)
+
     local function getNumberOfMobs()
         local beings = get_beings_in_rectangle(tileToPixel(16), tileToPixel(16), tileToPixel(16), tileToPixel(16))
         local mobcount=0
@@ -51,6 +54,8 @@ local function hungoriTalk(npc, ch)
             on_remove(ch, function() monster_remove(mob) end)
             on_death(mob, function() being_say(npc, "Whoot!") end)
         end
+        on_remove(ch, function() chr_set_quest(ch, "HungoriMaggots", "") end)
+
         chr_set_quest(ch, "HungoriMaggots", "Hunting")
         say("Just help me killing all maggots!")
     elseif res > 0 and res==maggotquest then
@@ -69,6 +74,30 @@ local function hungoriWaypointReached(npc)
     gotoNextWaypoint(npc)
 end
 
+local timer = 0
+local function hungoriUpdate(npc)
+
+    local function getNumberOfMobs()
+        local beings = get_beings_in_rectangle(tileToPixel(16), tileToPixel(16), tileToPixel(16), tileToPixel(16))
+        local mobcount=0
+        for i=1,#beings do
+            if being_type(beings[i]) == TYPE_MONSTER then
+                mobcount=mobcount+1
+            end
+        end
+        return mobcount
+    end
+
+    if timer > 10 then
+        timer = 0
+        if getNumberOfMobs() > 7 then
+            guard.lookForEnemy(npc)
+        end
+    else
+        timer = timer + 1
+    end
+end
+
 local hungoriWay = {
         {x=tileToPixel(21), y=tileToPixel(26), wait=5},
         {x=tileToPixel(20), y=tileToPixel(30)},
@@ -78,6 +107,11 @@ local hungoriWay = {
 
 local hungori = npc_create("Hungori", 217, GENDER_MALE,
                            tileToPixel(21), tileToPixel(26),
-                           hungoriTalk, nil)
+                           hungoriTalk, hungoriUpdate)
+
+guard.create(hungori, posX(hungori), posY(hungori),
+             5 * 32, 50, 25, 2 * 32, 999, DAMAGE_PHYSICAL,
+             ELEMENT_NEUTRAL, function(npc, b) return npc ~= b end)
+
 setWaypoints(hungori, hungoriWay, 3, hungoriWaypointReached)
 gotoNextWaypoint(hungori)
