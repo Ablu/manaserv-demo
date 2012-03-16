0b8c21383f4fa60002fa1d01bd828eb83d1251f2
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-03-16 21:43:46 +0100

    Added function for area spawn

--- scripts/global_functions.lua
+++ scripts/global_functions.lua
@@ -11,3 +11,16 @@ ATTRIBUTE_MOVEMENT_SPEED = 16
 function tileToPixel(v)
     return v * TILESIZE + TILESIZE / 2
 end
+
+function monster_area_spawn(id, x, y, w, h, n)
+    local monsters = {}
+    while n > 0 do
+        local r_x = math.random(x, x + w)
+        local r_y = math.random(y, y + h)
+        if is_walkable(r_x, r_y) then
+            table.insert(monsters, monster_create(id, r_x, r_y))
+            n = n - 1
+        end
+    end
+    return monsters
+end
